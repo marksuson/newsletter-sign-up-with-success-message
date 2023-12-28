f750584fa06edb4534739f6e428f86a040493268
Mark Suson <mark.suson@gmail.com>
2023-12-28 09:34:57 -0500

    adjusting format

--- success.js
+++ success.js
@@ -1,7 +1,6 @@
 const params = new URLSearchParams(window.location.search);
 const email = params.get("email");
 
-// Insert the email into the HTML
 const emailParagraph = document.querySelector("#success-message");
 emailParagraph.innerHTML = `A confirmation email has been sent to <strong>${email}</strong>. Please open it and click the link to confirm your subscription.`;
 
